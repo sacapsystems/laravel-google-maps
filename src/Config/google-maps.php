d7f494cc43389e5a8b1b2e17d94c8a15ad7a641c
clint-mclean <clint@sacap.edu.za>
2025-01-30 11:20:06 +0200

    Update Google Maps search URL to use autocomplete endpoint

--- src/Config/google-maps.php
+++ src/Config/google-maps.php
@@ -2,7 +2,7 @@
 
 return [
     'base_url' => [
-        'search' => env('GOOGLE_MAPS_SEARCH_URL', 'https://maps.googleapis.com/maps/api/place/textsearch/json'),
+        'search' => env('GOOGLE_MAPS_SEARCH_URL', 'https://maps.googleapis.com/maps/api/place/autocomplete/json'),
         'details' => env('GOOGLE_MAPS_DETAILS_URL', 'https://maps.googleapis.com/maps/api/place/details/json'),
     ],
     'api_key' => env('GOOGLE_MAPS_API_KEY'),
